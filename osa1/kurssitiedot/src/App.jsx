1150e4a741135c8461994c408782f55a331300dc
Juho <juho.keskinen@seamk.fi.007@gmail.com>
2026-08-27 18:35:35 +0300

    1.2

--- osa1/kurssitiedot/src/App.jsx
+++ osa1/kurssitiedot/src/App.jsx
@@ -2,14 +2,22 @@ const Header = (props) => {
 console.log(props)
 return <h1>{props.course}</h1>
 }
+
+
 const Content = (props) => {
   console.log(props)
   return <p>{props.part} {props.exercises} </p>
-
 }
+
+
 const Total = (props) => {
   console.log(props)
   return <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+}
+
+const Part = (props) => {
+  console.log(props)
+  return <p>{props.part} {props.exercises}</p>
 }
 
 const App = () => {
@@ -24,10 +32,9 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content part={part1} exercises={exercises1} />
-      <Content part={part2} exercises={exercises2} />
-      <Content part={part3} exercises={exercises3} />
-      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+      <Part part={part1} exercises={exercises1} />
+      <Part part={part2} exercises={exercises2} />
+      <Part part={part3} exercises={exercises3} />
     </div>
 
   )
