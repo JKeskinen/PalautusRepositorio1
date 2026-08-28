f4be36c478489b7253946595e7a191c18c402311
Juho <juho.keskinen.007@gmail.com>
2026-08-28 11:40:55 +0300

    t1.5 STEP5, JS-olio

--- osa1/kurssitiedot/src/App.jsx
+++ osa1/kurssitiedot/src/App.jsx
@@ -4,13 +4,13 @@
 //Total komponentti, joka ottaa vastaan propsin ja renderöi tehtävien kokonaismäärän
 
 const Header = (props) => {
-console.log(props)
+console.log("Header", props)
 return <h1>{props.course}</h1>
 }
 
-const Part = ({part, exercises}) =>(
+const Part = ({part}) =>(
   <p>
-    {part} {exercises}
+    {part.name} {part.exercises}
   </p>
 )
 
@@ -21,52 +21,53 @@ const Content = (props) => {
   console.log("Content", props)
   return (
   <div>
-    <Part part={props.part1.name} exercises={props.part1.exercises} />
-    <Part part={props.part2.name} exercises={props.part2.exercises} />
-    <Part part={props.part3.name} exercises={props.part3.exercises} />
+    <Part part={props.parts[0]} />
+    <Part part={props.parts[1]} />
+    <Part part={props.parts[2]} />
     </div>
   )
 }
 
 // Total komponentti, joka ottaa vastaan propsin ja renderöi tehtävien kokonaismäärän
 const Total = (props) => {
-  console.log(props)
-  return <p>Number of exercises: {props.part1.exercises + props.part2.exercises + props.part3.exercises}</p>
+  console.log("Total", props)
+
+  let sum = 0
+  for (let index = 0; index < props.parts.length; index++) {
+    sum += props.parts[index].exercises
+  }
+  return <p>Number of exercises: {sum}</p>
 }
 
 
 
 
 
-//App pääkomponentti t1.3 siirrytään käyttämään olioita
+//App pääkomponentti t1.4 listana
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
     
 
   // return, renderöi Header, Content ja Total komponentit.
   return (
     <div>
       <Header course={course} />
-      <Content 
-      part1={part1} 
-      part2={part2} 
-      part3={part3} />
-      <Total 
-      part1={part1} 
-      part2={part2} 
-      part3={part3} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   )
 
